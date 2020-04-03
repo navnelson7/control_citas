181c2dbcb26b83ff3fae10f679e5759032585afa
navnelson7 <navnelson7@gmailcom>
2020-04-02 23:13:39 -0600

    Error en el arreglo inicial del storage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React,{Fragment, useState} from 'react';
+import React,{Fragment, useState, useEffect} from 'react';
 import Formulario from './components/Formulario'
 import Cita from './components/Cita';
 function App() {
+  //citas en localstorages
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if(!citasIniciales){
+    citasIniciales = [];
+  }
+
   //agregando arreglo de citas
-  const [citas, guardarCitas] = useState([]);
+  const [citas, guardarCitas] = useState([citasIniciales]);
+  
+
+  //use effect para realizar ciertas operaciones cuando el state cambie
+  useEffect(() => {
+    if(citasIniciales){
+      localStorage.setItem('citas',JSON.stringify(citas))
+    } else{
+      localStorage.setItem('citas',JSON.stringify([]));
+    }
+  }, [citas])
 
   //funcion que tome cita actual y ponga las nuevas
   const crearCita = cita => {
@@ -13,6 +29,14 @@ function App() {
     ]);
   }
 
+  //funcion que elimina cita por su id
+  const eliminarCita =  id =>{
+    const nuevasCitas = citas.filter( cita => cita.id !== id)
+    guardarCitas(nuevasCitas)
+  }
+
+  //mensaje condicional
+  const titulo = citas.length === 0 ? 'No hay Citas' : 'Administra tus citas';
   return (
     <Fragment>
       <h1>Administrador de Pacientes</h1>
@@ -24,11 +48,12 @@ function App() {
             />
           </div>
           <div className="one-half column">
-            <h2>Administra tus citas</h2>
+            <h2>{titulo}</h2>
             {citas.map(cita =>(
               <Cita 
                 key={cita.id}
                 cita={cita}
+                eliminarCita={eliminarCita}
               />
             ))}
           </div>
--- src/components/Cita.js
+++ src/components/Cita.js
@@ -1,7 +1,16 @@
 import React from 'react';
-const Cita = () => (
+const Cita = ({cita,eliminarCita}) => (
     <div className="cita">
-        <p>Mascota: <span></span></p>
+        <p>Mascota: <span>{cita.mascota}</span></p>
+        <p>Propietario: <span>{cita.propietario}</span></p>
+        <p>Fecha: <span>{cita.fecha}</span></p>
+        <p>Hora: <span>{cita.hora}</span></p>
+        <p>Sintomas: <span>{cita.sintomas}</span></p>
+        <button
+            className="button eliminar u-full-width"
+            //on click
+            onClick={() => eliminarCita(cita.id)}
+        >Eliminar</button>
     </div>
 );
  
